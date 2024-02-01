0ea44a210371d0fb07891492b3048c7df4e26b89
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-02-01 11:16:19 +0500

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,24 +2,24 @@ import Link from "next/link";
 import { DateRange } from "react-day-picker";
 import { Button } from "@/components/ui/button";
 
-const page = async () => {
-  // const url =
-  //   process.env.NEXT_PUBLIC_API_URL +
-  //   "/search?" +
-  //   "from=" +
-  //   searchParams.from +
-  //   "&to=" +
-  //   searchParams.to;
+const page = async ({ searchParams }: { searchParams: DateRange }) => {
+  const url =
+    process.env.NEXT_PUBLIC_API_URL +
+    "/search?" +
+    "from=" +
+    searchParams.from +
+    "&to=" +
+    searchParams.to;
 
-  // const response = await fetch(url, {
-  //   method: "POST",
-  // });
+  const response = await fetch(url, {
+    method: "POST",
+  });
 
-  // const { message, data } = await response.json();
+  const { message, data } = await response.json();
 
   return (
     <div className="flex flex-col items-center justify-center space-y-28 p-4">
-      {/* <div className="w-full space-y-4">
+      <div className="w-full space-y-4">
         <h1 className="text-2xl font-bold ">Positive Velocity</h1>
         <div className="rounded-lg bg-slate-400/30 p-4 shadow-md">
           {data.positiveVelocity}
@@ -33,7 +33,7 @@ const page = async () => {
       </div>
       <Button>
         <Link href="/">Return to Search</Link>
-      </Button> */}
+      </Button>
     </div>
   );
 };
