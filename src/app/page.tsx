e83d05185ffcaf7bbc25b7bde19ac5a9f5b40253
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-02-02 15:40:16 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SearchForm from "@/components/forms/SearchForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="space-y-10 rounded-lg border border-white/20 p-36">
+      <div className="space-y-10 rounded-lg border border-white/15 p-36 shadow-2xl shadow-white/30">
         <FileUploadForm />
         <SearchForm />
       </div>
